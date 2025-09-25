0bcde20ceec4df298a645d6255d15c00b5faa6cf
Sushmita <sushmitakumari363@gmail.com>
2025-09-25 14:59:06 +0530

    removed unwanted library

--- src/scenes/global/CustomSidebar.jsx
+++ src/scenes/global/CustomSidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Sidebar, Menu, MenuItem, ProSidebarProvider } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
