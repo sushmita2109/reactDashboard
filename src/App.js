91486efefa124632939cfb603a289157c9279582
Sushmita <sushmitakumari363@gmail.com>
2025-09-25 10:17:17 +0530

    calender added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar/calendar";
+import Calender from "./scenes/calender";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -37,7 +37,7 @@ function App() {
                 <Route path="/pie" element={<Pie />} />
                 <Route path="/line" element={<Line />} />
                 <Route path="/faq" element={<FAQ />} />
-                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/calendar" element={<Calender />} />
                 <Route path="/geography" element={<Geography />} />
               </Routes>
             </main>
